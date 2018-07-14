e61c39eb93bcdc58621c6619d5574730592a0064
Alexandre Van Besien <alexandre.vanbesien@gmail.com>
2018-07-14 15:42:43 +0200

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_14_084142) do
+ActiveRecord::Schema.define(version: 2018_07_14_103054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "description"
+    t.bigint "user_id"
+    t.bigint "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_answers_on_topic_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_topics_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +50,7 @@ ActiveRecord::Schema.define(version: 2018_07_14_084142) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "topics"
+  add_foreign_key "answers", "users"
+  add_foreign_key "topics", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+roxana = User.new(email: 'roxy@gmail.com', password: '123456')
+roxana.save
+alex = User.new(email: 'alex@gmail.com', password: '123456')
+alex.save
+
+puts "user created"
+
+Topic.destroy_all
+
+
+robe = Topic.new(description: 'looking this kind of dress', user_id: roxana.id)
+robe.save
+
+puts "topic created"
+
+Answer.destroy_all
+
+
+answer = Answer.new(description: 'www.zara.com', user_id: alex.id, topic_id: robe.id)
+answer.save
+puts "answer created"
